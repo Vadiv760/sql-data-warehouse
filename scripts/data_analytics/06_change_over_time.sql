7abcc961fc1c86fa412fea13e56eac89a7e7f4bc
Vadiv760 <kubadoruch670@gmail.com>
2026-05-29 18:20:17 +0200

    Create 06_change_over_time.sql

---
+++ scripts/data_analytics/06_change_over_time.sql
@@ -0,0 +1,23 @@
+SELECT
+YEAR(order_date) AS order_year,
+MONTH(order_date) AS order_month,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULl
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date)
+
+SELECT
+FORMAT(order_date, 'yyyy-MMM') AS order_date,
+-- DATETRUNC(YEAR, order_date) AS order_date,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULl
+-- GROUP BY DATETRUNC(YEAR, order_date)
+-- ORDER BY DATETRUNC(YEAR, order_date)
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
